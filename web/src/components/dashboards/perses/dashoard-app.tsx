7084a53a6bf1796e401af7d3bffbaff59d3c4b10
Jenny Zhu <jenny.a.zhu@gmail.com>
2025-11-18 15:14:03 -0500

    Add DataViewToolbar and DataViewTable

--- web/src/components/dashboards/perses/dashoard-app.tsx
+++ web/src/components/dashboards/perses/dashoard-app.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, ReactNode, useState } from 'react';
+import { ReactElement, ReactNode, useMemo, useState } from 'react';
 import { Box } from '@mui/material';
 import { ChartsProvider, ErrorAlert, ErrorBoundary, useChartsTheme } from '@perses-dev/components';
 import { DashboardResource, EphemeralDashboardResource } from '@perses-dev/core';
@@ -22,6 +22,140 @@ import {
   useEditMode,
 } from '@perses-dev/dashboards';
 import { OCPDashboardToolbar } from './dashboard-toolbar';
+
+import { Pagination } from '@patternfly/react-core';
+import { BulkSelect } from '@patternfly/react-component-groups';
+import { DataViewToolbar } from '@patternfly/react-data-view/dist/dynamic/DataViewToolbar';
+import { ResponsiveAction, ResponsiveActions } from '@patternfly/react-component-groups';
+import { DataViewFilters } from '@patternfly/react-data-view/dist/dynamic/DataViewFilters';
+import { DataViewTextFilter } from '@patternfly/react-data-view/dist/dynamic/DataViewTextFilter';
+import { useDataViewPagination } from '@patternfly/react-data-view/dist/dynamic/Hooks';
+import { DataView } from '@patternfly/react-data-view/dist/dynamic/DataView';
+import { DataViewTable } from '@patternfly/react-data-view/dist/dynamic/DataViewTable';
+
+const TableToolBar = () => {
+  return (
+    <DataViewToolbar
+      clearAllFilters={() => console.log('clearAllFilters called')}
+      filters={
+        <DataViewFilters onChange={() => console.log('onSetFilters calles')} values={{}}>
+          <DataViewTextFilter filterId="name" title="Name" placeholder="Filter by name" />
+          <DataViewTextFilter filterId="branch" title="Branch" placeholder="Filter by branch" />
+        </DataViewFilters>
+      }
+    />
+  );
+};
+
+const perPageOptions = [
+  { title: '5', value: 5 },
+  { title: '10', value: 10 },
+];
+
+interface Repository {
+  name: string;
+  branches: string | null;
+  prs: string | null;
+  workspaces: string;
+  lastCommit: string;
+}
+
+const repositories: Repository[] = [
+  {
+    name: 'Repository one',
+    branches: 'Branch one',
+    prs: 'Pull request one',
+    workspaces: 'Workspace one',
+    lastCommit: 'Timestamp one',
+  },
+  {
+    name: 'Repository two',
+    branches: 'Branch two',
+    prs: 'Pull request two',
+    workspaces: 'Workspace two',
+    lastCommit: 'Timestamp two',
+  },
+  {
+    name: 'Repository three',
+    branches: 'Branch three',
+    prs: 'Pull request three',
+    workspaces: 'Workspace three',
+    lastCommit: 'Timestamp three',
+  },
+  {
+    name: 'Repository four',
+    branches: 'Branch four',
+    prs: 'Pull request four',
+    workspaces: 'Workspace four',
+    lastCommit: 'Timestamp four',
+  },
+  {
+    name: 'Repository five',
+    branches: 'Branch five',
+    prs: 'Pull request five',
+    workspaces: 'Workspace five',
+    lastCommit: 'Timestamp five',
+  },
+  {
+    name: 'Repository six',
+    branches: 'Branch six',
+    prs: 'Pull request six',
+    workspaces: 'Workspace six',
+    lastCommit: 'Timestamp six',
+  },
+];
+
+const rows = repositories.map((item) => Object.values(item));
+
+const columns = ['Repositories', 'Branches', 'Pull requests', 'Workspaces', 'Last commit'];
+
+const ouiaId = 'LayoutExample';
+
+const MyTable: React.FunctionComponent = () => {
+  const pagination = useDataViewPagination({ perPage: 5 });
+  const { page, perPage } = pagination;
+
+  const pageRows = useMemo(
+    () => rows.slice((page - 1) * perPage, (page - 1) * perPage + perPage),
+    [page, perPage],
+  );
+  return (
+    <DataView>
+      <TableToolBar />
+      <DataViewToolbar
+        ouiaId="DataViewHeader"
+        pagination={
+          <Pagination
+            perPageOptions={perPageOptions}
+            itemCount={repositories.length}
+            {...pagination}
+          />
+        }
+      />
+      <DataViewTable
+        aria-label="Repositories table"
+        ouiaId={ouiaId}
+        columns={columns}
+        rows={pageRows}
+      />
+      <DataViewToolbar
+        ouiaId="DataViewFooter"
+        pagination={
+          <Pagination
+            isCompact
+            perPageOptions={perPageOptions}
+            itemCount={repositories.length}
+            {...pagination}
+          />
+        }
+      />
+    </DataView>
+  );
+};
+
+const HelloWorld = () => {
+  return <h1> Hello world!!! </h1>;
+};
 
 export interface DashboardAppProps {
   dashboardResource: DashboardResource | EphemeralDashboardResource;
@@ -109,7 +243,14 @@ export const OCPDashboardApp = (props: DashboardAppProps): ReactElement => {
         flexDirection: 'column',
       }}
     >
-      <OCPDashboardToolbar
+      <h1> --------- TESTING ----------- </h1>
+
+      <MyTable />
+
+      <h1> Hello World !! </h1>
+      <h1> --------- TESTING ----------- </h1>
+
+      {/* <OCPDashboardToolbar
         dashboardName={dashboardResource.metadata.name}
         initialVariableIsSticky={isInitialVariableSticky}
         onSave={onSave}
@@ -140,7 +281,7 @@ export const OCPDashboardApp = (props: DashboardAppProps): ReactElement => {
         {isLeavingConfirmDialogEnabled &&
           isEditMode &&
           (LeaveDialog({ original: originalDashboard, current: dashboard }) as ReactElement)}
-      </Box>
+      </Box> */}
     </Box>
   );
 };
